28552ac01d6bc4e84b42b71e8494da7cad3ec7ad
mugikohaone-netizen <mugikoha.one@gmail.com>
2026-07-03 22:00:10 +0900

    🔄 SW更新 vmr4xxxcq

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'app-mr4xjjwn';
+const CACHE = 'app-mr4xxxcq';
 const FILES = ['./','./index.html'];
 self.addEventListener('install',e=>{
   e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)).then(()=>self.skipWaiting()));
