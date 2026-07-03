92461101e83a091db1b18d633c147e5183cb136e
mugikohaone-netizen <mugikoha.one@gmail.com>
2026-07-03 16:53:09 +0900

    🔄 SW更新 vmr4mz45j

---
+++ sw.js
@@ -0,0 +1,17 @@
+const CACHE = 'app-mr4mz45j';
+const FILES = ['./','./index.html'];
+self.addEventListener('install',e=>{
+  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)).then(()=>self.skipWaiting()));
+});
+self.addEventListener('activate',e=>{
+  e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));
+});
+self.addEventListener('fetch',e=>{
+  const url = new URL(e.request.url);
+  const isSameOrigin = url.origin === self.location.origin;
+  if(!isSameOrigin || e.request.method !== 'GET'){
+    e.respondWith(fetch(e.request));
+    return;
+  }
+  e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
+});
